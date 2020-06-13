5144c92eba210ea8158fc8fbbe87fc28376b85d3
German Yakimov <german13yakimov@gmail.com>
2020-06-14 05:38:49 +0700

    Final commit for homework 4.

--- databases/work_with_database/phones/models.py
+++ databases/work_with_database/phones/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Phone(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.TextField()
+    name = models.TextField(max_length=256)
     price = models.FloatField()
     image = models.TextField()
     release_date = models.DateField()
     lte_exists = models.BooleanField()
-    slug = models.TextField()
+    slug = models.TextField(max_length=256)
