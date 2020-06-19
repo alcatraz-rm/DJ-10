739d087b970b9a14fe06fac787cdcf799b263cd5
German Yakimov <german13yakimov@gmail.com>
2020-06-19 19:34:53 +0700

    Add prefetch-related to second task.

--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -11,6 +11,6 @@ def students_list(request):
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
-    context['students_list'] = Student.objects.all().order_by(ordering)
+    context['students_list'] = Student.objects.all().order_by(ordering).prefetch_related('teachers')
 
     return render(request, template, context)
