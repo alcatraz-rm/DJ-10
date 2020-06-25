dfe3b94ebcdfa5f0117d5b96d19ff11c943b3a80
German Yakimov <german13yakimov@gmail.com>
2020-06-25 21:50:30 +0700

    Fix bug in the first task (is_main field in template).

--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -18,7 +18,7 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
-    scopes = models.ManyToManyField(Scope, through='ArticleShip')
+    scopes = models.ManyToManyField(Scope, through='ArticleShip', through_fields=('article', 'scope', 'is_main'))
 
     class Meta:
         verbose_name = 'Статья'
@@ -32,6 +32,9 @@ class ArticleShip(models.Model):
     article = models.ForeignKey(Article, related_name='articles', verbose_name='Статья', on_delete=models.CASCADE)
     scope = models.ForeignKey(Scope, related_name='scopes', verbose_name='Категория', on_delete=models.CASCADE)
     is_main = models.BooleanField(verbose_name='Основная категория')
+
+    class Meta:
+        ordering = ('-is_main', 'scope__name')
 
     def __str__(self):
         return f'{self.article}_{self.scope}'
--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from articles.models import Article
+from articles.models import Article, ArticleShip
 
 
 def articles_list(request):
@@ -10,6 +10,17 @@ def articles_list(request):
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
-    context['object_list'] = Article.objects.all().order_by(ordering)
+
+    context['articles_list'] = []
+
+    for article in Article.objects.all().order_by(ordering):
+        article_object = {'article': article}
+
+        for scope in article.scopes.all():
+            if ArticleShip.objects.filter(article_id=article.id, scope_id=scope.id).first().is_main:
+                article_object['main_scope_id'] = scope.id
+                break
+
+        context['articles_list'].append(article_object)
 
     return render(request, template, context)
